470bd6d57bd024d9af493e80230b613056c9182f
brunomry <brunomadozzo@gmail.com>
2023-11-09 11:43:22 -0300

    Resolvi el ejercicio N°1-TP3. Fusione el navbar. Tambien agregue una grilla y estilos con clases de bootstrap para mostrar los resultados.

--- JS/ejercicio1-tp3.js
+++ JS/ejercicio1-tp3.js
@@ -1,16 +1,42 @@
-document.write(`<main class="p-5"><h2 class="pb-3">Resolución de Ejercicio N°1</h2>`);
 
-let edad = prompt(`Ser mayor de edad para conducir \n Ingrese una edad:`);
+let elementoMain = document.getElementById("main");
+let elementoSection = `<section class="container">
+                          <h2 class="pb-3 text-center text-primary">Resolución de Ejercicio N°1 - TP3</h2>
+                          <div class="row">`;
 
-if(edad === ""){
-  document.write(`<p>No se ingresó ninguna edad correspondiente a una persona</p>`);
-}else{
-  edad = parseInt(edad);
-  (isNaN(edad)) 
-    ? document.write(`<p>La edad ingresada no corresponde a un número</p>`)
-    : (edad >= 18)     
-        ? document.write(`<p>La persona es mayor de edad ya que tiene <span class="fw-bold">${edad}</span> años, por lo tanto <span class="fw-bold">puede conducir</span></p>`)
-        : document.write(`<p>La persona es menor de edad ya que tiene <span class="fw-bold">${edad}</span> años, por lo tanto <span class="fw-bold">no puede conducir</span></p>`);
+let meses = [];
+
+//Solucion N°1
+elementoSection += `<article class="col-md-6">
+                      <h3>Solución N°1:</h3>
+                      <h4>Lista de meses</h4>`;
+
+elementoSection += `<ul>`
+let mes = "";
+//este for aplica para ambas soluciones
+for (let i = 0; i < 12; i++) {
+  do{
+    mes = prompt(`Listado de los 12 meses del año \n Ingrese el mes ${i+1}:`);
+    if(mes === null) alert(`La operación fue cancelada`);
+  }while(mes === "");
+  mes = mes.charAt(0).toUpperCase() + mes.slice(1);
+  meses.push(mes);
 }
 
-document.write(`</main>`);
+for (let i = 0; i < meses.length; i++) {
+  elementoSection += `<li>${meses[i]}</li>`
+}
+elementoSection += `</ul></article>`;
+
+
+//Solucion N°2 usando metodo map
+elementoSection += `<article class="col-md-6">
+                      <h3>Solución N°2:</h3>
+                      <h4>Lista de meses</h4>`;
+
+elementoSection += `<ul>`;
+meses.map(mes => elementoSection += `<li>${mes}</li>`);
+elementoSection += `</ul></article>`;
+
+elementoSection += `</row></div></section>`;
+elementoMain.innerHTML = elementoSection;
